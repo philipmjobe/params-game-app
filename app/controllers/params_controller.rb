d5f8b6c4afb7fe1bd448633b2cdb6fb317dce70c
Philip <philipmjobe@gmail.com>
2021-10-14 18:06:15 -0400

    copied the stuff from todays class for the controller off github to get a better understanding

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,18 +1,19 @@
 class ParamsController < ApplicationController
-  def 
-    name = params["my_name"]
-    if name == "A"
+  def name 
+    input_name = params["query_name"]
+    output_message = input_name.upcase
+    if output_message[0] == "A"
       render json: {message: "Hey, your name starts with the first letter of the alphabet!"}
     else 
-      render json: {message: "#{name.upcase}"}
+      render json: {message: output_message}
     end
   end
 
-  def sum
-    num1 = params["num1"].to_i
-    num2 = params["num2"].to_i
+  def game 
+    num1 = params[:variable1].to_i
+    num2 = params[:variable2].to_i
     sum = num1 + num2
-    render json: {message: "The total is #{sum}"}
+    render json: {message: sum}
   end
 
   def guess_query
@@ -26,5 +27,22 @@ class ParamsController < ApplicationController
       output_message = "Good job!"
     end
     render json: {message: output_message}
+  end 
+
+  def query_params
+    input_value = params["my_message"]
+    second_input = params["second_message"]
+    render json: { message: "The messages are #{input_value} and #{second_input}"}
+  end
+
+  def segment_params
+    input_value = params["this_is_a_variable"]
+    input_value2 = params["hot_dog"]
+    render json: {message: "The url segments are #{input_value} and #{input_value2}"}
+  end
+
+  def body_params
+    input_value = params["secret_info"]
+    render json: {message: "The secret info is #{input_value}"}
   end 
 end
