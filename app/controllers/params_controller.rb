0b221720b2432d34207ddff73eef044db147f659
Philip <philipmjobe@gmail.com>
2021-10-14 17:52:14 -0400

    copied the stuff from todays class off github to get a better understanding

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,5 +1,5 @@
 class ParamsController < ApplicationController
-  def name 
+  def 
     name = params["my_name"]
     if name == "A"
       render json: {message: "Hey, your name starts with the first letter of the alphabet!"}
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/query_params" => "params#query_params"
   get "/name" => "params#name"
-  get "/sum_game/:num1/:num2" => "params#sum"
-  get "/guess_query" => "params#guess_query"
-  post "/secret" => "params#secret"
+  get "/guess_query" => "params_examples#guess_query"
+  get "/segment_params/:this_is_a_variable/:hot_dog" => "params_examples#segment_params"
+  get "/game/:variable1/:variable2" => "params_examples#game"
+  get "/guess_query/:the_users_guess" => "params_example#guess_query"
+
 end
