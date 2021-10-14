862c87012825d955492eaf4eb06aae29d3e00bb4
Philip <philipmjobe@gmail.com>
2021-10-14 13:31:58 -0400

    alterted the if statement and the first json message

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,8 +1,8 @@
 class ParamsController < ApplicationController
   def name 
     name = params["my_name"]
-    if name == "a"
-      render json: {message: "Hey #{name.upcase}, yout name starts with the first letter of the alphabet!"}
+    if name == "A"
+      render json: {message: "Hey, your name starts with the first letter of the alphabet!"}
     else 
       render json: {message: "#{name.upcase}"}
     end
