be9683743dec62dcf2fdbef0ce491f825c6a196b
Philip <philipmjobe@gmail.com>
2021-10-14 14:12:10 -0400

    added wildcard get route and def sum

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -6,5 +6,11 @@ class ParamsController < ApplicationController
     else 
       render json: {message: "#{name.upcase}"}
     end
+
+    def sum
+      num1 = params["num1"].to_i
+      num2 = params["num2"].to_i
+      sum = num1 + num2
+      render json: {message: "The total is #{sum}"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/name" => "params#name"
-  
+  get "/sum_game/:num1/:num2" => "params#sum"
 end
