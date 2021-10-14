8386aea1c3f9843ef56331e45a12b835f1e2d34d
Philip <philipmjobe@gmail.com>
2021-10-14 13:00:03 -0400

    added a route and controller actions

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,2 +1,9 @@
 class ParamsController < ApplicationController
+  def name 
+    name = params["my-name"]
+    if name.upcase.first = "A"
+      render json: {message: "Hey #{name.upcase}, yout name starts with the first letter of the alphabet!"}
+    else 
+      render json: {message: "#{name.upcase}"}
+    end
 end
